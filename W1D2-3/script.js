c21e56991f48f088d38c62db321a94414cd805e1
davidjfk <davidjfk@hotmail.com>
2021-03-09 13:29:12 +0100

    Add files via upload
    Opdracht: JS programma draaien op jouw computer + comments maken

---
+++ W1D2-3/script.js
@@ -0,0 +1,22 @@
+//deel 1:
+console.log("Hallo Winc Academy studenten");
+
+// Dit is een grote som.
+// deel 2: vraag 1:
+console.log("deel 2: antwoord 1:");
+const multiplyTwoNumbers = (number1, number2) => { return (number1 * number2) };
+let multiplyResult = multiplyTwoNumbers(1230941, 1823794);
+console.log(multiplyResult);
+
+// deel 2: vraag 2:
+console.log("deel 2: antwoord 2:");
+const divideTwoNumbers2 = (number1, number2) => console.log(number1 / number2);
+divideTwoNumbers2(637263, 54);
+
+// deel 2: vraag 3:
+console.log("deel 2: antwoord 4:");
+console.log("Commments worden niet getoond in de console");
+
+// deel 2: vraag 6:
+console.log("deel 2: antwoord 6:");
+console.log("Een blok comment zet de code a.h.w. uit, waardoor de uitkomst van de som niet langer zichtbaar is");
