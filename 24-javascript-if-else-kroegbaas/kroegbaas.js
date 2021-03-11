ef7d590d69a811cb333df0c6e6f10d69a010c754
davidjfk <davidjfk@hotmail.com>
2021-03-11 10:24:32 +0100

    Add files via upload
    Assignment: javascript: kroegbaas: use if-else-statements

---
+++ 24-javascript-if-else-kroegbaas/kroegbaas.js
@@ -0,0 +1,35 @@
+// Deel 1:
+console.log("deel 1:");
+const age = 35;
+
+
+
+if (age > 17) {
+    console.log("Welcome, please enter and enjoy the party")
+}
+else {
+    console.log("Please stay outside, because you are too young. We apologize for the inconvenience.")
+}
+
+// Deel 2:
+console.log("deel 2:");
+const isFemale = true;
+
+if (isFemale) {
+    console.log("Welcome to Ladies Night. Please buy a ticket.")
+}
+else {
+    console.log("Sorry, it is Ladies Night: so you cannot buy a ticket.")
+}
+
+// Deel 3:
+console.log("deel 3:");
+const driverStatus = 'bob';
+
+
+if (driverStatus === 'bob') {
+    console.log("You may drive a vehicle, because you have not drunk any alcohol.")
+}
+else {
+    console.log("You may NOT drive a vehicle, because you have drunk alcohol.")
+}
