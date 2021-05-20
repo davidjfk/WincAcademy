ffd5dc03478daadf9cbce95d705b379ae088cdb9
David Sneek <davidjfk@hotmail.com>
2021-05-20 12:46:33 +0200

    js-basic-arrow-functions. David Sneek

---
+++ 38-js-basic-arrow-functions/basic-arrow-functions.js
@@ -0,0 +1,24 @@
+console.log("David Sneek: JS - basic-arrow-functions. ")
+console.log("deel A: ");
+
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+ikRockArrowFunctions()
+
+
+console.log("deel B: ");
+const fivePlusSeven = () => 5 + 7;
+console.log(fivePlusSeven());
+
+
+console.log("deel C: ");
+const myFunction = (a , b) => a + b;
+console.log(myFunction(3, 8));
+
+console.log("deel D: ");
+const addFive = a => a + 5;
+console.log(addFive(4));
+
+
+console.log("deel E: ");
+const createObject = () => ({greeting: "hoi"})
+console.log(createObject());
